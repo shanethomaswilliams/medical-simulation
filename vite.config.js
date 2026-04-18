efd900e5a8eaf742bf805397d7c4566e794fd695
Shane Williams <46091293+shanethomaswilliams@users.noreply.github.com>
2026-04-18 18:30:37 -0400

    added config for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/taiwan-casualty-model/", // <-- use your repo name here
+  base: "/medical-simulation/", // <-- use your repo name here
 });
